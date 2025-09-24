3df7298fd72168a4ee0818fee728f55bdbb391de
Olive Musca <olivemusca@gmail.com>
2025-09-24 12:13:34 +0800

    feat(behind-the-scenes): complete Hour 1 with scoping and hoisting examples

--- 07-behind-the-scenes/starter/script.js
+++ 07-behind-the-scenes/starter/script.js
@@ -1,1 +1,49 @@
 'use strict';
+
+//function alpha() {
+    //console.log('alpha:start');
+   // beta();
+    //console.log('alpha:end');
+//}
+
+//function beta() {
+ //   console.log('beta');
+//}
+
+//alpha();
+
+//const outer = 'global';
+
+//function demoScope() {
+    //const inner = 'function';
+    //if (true) {
+        //const blockConst = 'block';
+        //var functionVar = 'var-function-scoped';
+        //console.log(outer, inner, blockConst, functionVar);
+    //}
+
+    //console.log(outer, inner, functionVar);
+//}
+//demoScope();
+
+console.log(varX); // undefined
+//console.log(letX); // ReferenceError
+//console.log(constX); // ReferenceError
+
+var varX = 1;
+let letX = 2;
+const constX = 3;
+
+console.log(addDec1(2, 3)); // 5
+console.log(addExpr(2, 3)); // 5
+
+function addDec1(a, b) {
+    return a + b;
+}
+
+const addExpr = function(a, b) {
+    return a + b;
+};
+
+const addArrow = (a, b) => a + b;
+
